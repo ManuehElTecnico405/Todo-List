00a2724299f08d3f88d09f1c23701068a4521f81
ManuehElTecnico405 <olivertorrulo@gmail.com>
2025-02-24 13:21:33 +0100

    EDIT_THING

--- source/js/script.js
+++ source/js/script.js
@@ -35,9 +35,10 @@ function addCurrentTask(name,desc)
     CurrentTasks.innerHTML+=
         `<div class="task cur_task">
         <input id="checkTask" class="delete_task" type="submit" value="" onclick="completeTask(this.parentNode)">
-        <input type="button" value="" id="edit_button" onclick="editTask(this.parenyNode)">
+        <input type="button" value="" id="edit_button"onclick="openEdit(this.parentNode)">
         <input class="select_task" type="checkbox">
-        <p>${name}</p><p>${desc}</p>
+        <h2 id="CT">${name}</h2>
+        <p id="CD">${desc}</p>
         </label>
         </div>`;
 }
@@ -57,45 +58,35 @@ function completeTask(x)
 {
     x.classList.replace("cur_task","com_task");
     CompletedTasks.appendChild(x);
-    console.log(x);
 }
 
-function deleteAllTasks(){
+function deleteAllTasks()
+{
     CurrentTasks.innerHTML = ""
     CompletedTasks.innerHTML = ""
     deleteAllTasks.innerHTML = ""
-
 }
 
-function editTask (taskElement){
-    let currentText = taskElement.childNodes[3].nodeValue
-    let inputField = document.createElement("input")
-    inputField.type = "text"
-    inputField.value = currentText
-    let saveButton = document.createElement("input")
-    saveButton.type = "button"
-    saveButton.value = "Guardar"
-    saveButton.onclick = function(){
-        taskElement.childNodes[3].nodeValue = inputField.value
-        taskElement.removeChild(inputField)
-        taskElement.removeChild(saveButton)
-        alert("¡Cambio guardado correctamente!")
-    }
-    taskElement.replaceChild(inputField, taskElement.childNodes[3])
-    taskElement.appendChild(saveButton)
-}
+// BOTON EDITAR
 
-//html
-//texto en botones (añadir y borrar) VV
-//Falta el icono de completar VV
-//Posicion de las barras de tarea VV
+function openEdit(x)
+{
+    //let curName = x.getElementById('CT');let curDesc = x.getElementById('CD');
+    document.getElementById('id01').style.display='block';
+    let newTitle = document.getElementById('newTitle');
+    let newDesc = document.getElementById('newDesc');
+    newTitle.value="";
+    newDesc.value="";
+}
+function sendEdit(mode){document.getElementById('id01').style.display='none';}
+
+//switch(mode){case "CONFIRM": ;break;case "CANCEL": return 0;}
+
 
 //css
-//Mejorar diseño del header
 //Añadir fondo difuminado (o algo pa decorar)
 
 //js
-//Boton de Editar
 //Boton Seleccionar todo (current, completed & trashed)
 //Reordenar mejor botones (Seguramente se requiera css)
 
@@ -104,3 +95,20 @@ function editTask (taskElement){
 // function addCurrentTask(task){currentTasks.innerHTML+=}
 // deletedTasks.forEach(task => {task.remove();});
 //function deleteAllTasks.forEach(task => {task.remove();});
+
+
+//let currentText = taskElement.childNodes[3].nodeValue
+//let inputField = document.createElement("input")
+//inputField.type = "text"
+//inputField.value = currentText
+//let saveButton = document.createElement("input")
+//saveButton.type = "button"
+//saveButton.value = "Guardar"
+//saveButton.onclick = function(){
+//    taskElement.childNodes[3].nodeValue = inputField.value
+//    taskElement.removeChild(inputField)
+//    taskElement.removeChild(saveButton)
+//    alert("¡Cambio guardado correctamente!")
+//}
+//taskElement.replaceChild(inputField, taskElement.childNodes[3])
+//taskElement.appendChild(saveButton
